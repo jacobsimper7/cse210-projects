9d67763cd21f985549427c6b320b62c2f71f7a67
jacobsimper7 <jacobsimper777@gmail.com>
2025-05-12 09:32:57 -0700

    Exercise 3 Core Requirements done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Console.Write("What is the magic number? ");
+        // string magicNumberInput = Console.ReadLine();
+        // int number = int.Parse(magicNumberInput);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        int guess = 0;
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            string guessInput = Console.ReadLine();
+            guess = int.Parse(guessInput);
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else
+            {
+                Console.WriteLine("Too high!");
+            }
+        }
     }
 }
