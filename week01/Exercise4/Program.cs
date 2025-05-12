847d8056e5d011902df9b6e76690f2f80440f651
jacobsimper7 <jacobsimper777@gmail.com>
2025-05-12 15:10:40 -0700

    Exercise 4 Core Requirements

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 wehen finished.");
+        List<int> numbers = new List<int>();
+        int number = 1;
+        int sum = 0;
+        int greatest = 0;
+        int smallest = 100;
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+            // Check if the number is not zero before adding it to the list
+            if (number != 0)
+            {
+                numbers.Add(number);
+                sum += number;
+            }
+            // Check if the number is greater than the current greatest number
+            // and if it is not zero
+            if (number < smallest && number != 0)
+            {
+                smallest = number;
+            }
+            else if (number > greatest)
+            {
+                greatest = number;
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is {sum / numbers.Count}.");
+        Console.WriteLine($"The greatest number is {greatest}.");
+        Console.WriteLine($"The smallest number is {smallest}.");
     }
 }
