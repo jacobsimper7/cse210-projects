e9075f6f27ad936207b645b5a23e26b7d34495ce
jacobsimper7 <jacobsimper777@gmail.com>
2025-05-12 14:28:58 -0700

    Exersize Core done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -8,11 +8,21 @@ class Program
         // string magicNumberInput = Console.ReadLine();
         // int number = int.Parse(magicNumberInput);
 
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 11);
+        
+        string play = "yes";
+        Console.WriteLine("Welcome to the Number Guessing Game!");
+        Console.WriteLine("I have selected a number between 1 and 10. Can you guess it?");
 
-        int guess = 0;
-        while (guess != number)
+        while (play == "yes")
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+            int guess = 0;
+            int numberOfGuesses = 0;
+            Console.Write("Would you like to play the game? (yes/no) ");
+            string playInput = Console.ReadLine();
+            play = playInput.ToLower();
+            while (guess != number)
         {
             Console.Write("What is your guess? ");
             string guessInput = Console.ReadLine();
@@ -20,15 +30,21 @@ class Program
             if (guess == number)
             {
                 Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You guessed {numberOfGuesses} times.");
             }
             else if (guess < number)
             {
+                numberOfGuesses++;
+                Console.WriteLine($"You have guessed {numberOfGuesses} times.");
                 Console.WriteLine("Too low!");
             }
             else
             {
+                numberOfGuesses++; 
+                Console.WriteLine($"You have guessed {numberOfGuesses} times.");
                 Console.WriteLine("Too high!");
             }
         }
+        }        
     }
 }
