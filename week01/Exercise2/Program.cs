4dee4f7707342d684a7c9fa4e0dbcea69206a1a9
jacobsimper7 <jacobsimper777@gmail.com>
2025-05-12 09:14:18 -0700

    Finished Exercise2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+
+        int lastDigit = grade % 10;
+        string gradeSign = "";
+        // Determine the grade sign based on the last digit
+        if (lastDigit >= 7)
+        {
+            gradeSign = "+";
+        }
+        else if (lastDigit <= 3)
+        {
+            gradeSign = "-";
+        }
+        else
+        {
+            gradeSign = "";
+        }
+
+        if (grade >= 90)
+        {
+            Console.WriteLine($"You got an A!");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine($"You got a {gradeSign}B!");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine($"You got a {gradeSign}C!");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine($"You got a {gradeSign}D!");
+        }
+        else
+        {
+            Console.WriteLine($"You got an F.");
+        }
     }
 }
